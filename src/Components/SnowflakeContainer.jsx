f00950b823d42cbb7fafffef52dccb8f4fda5f92
smuke <smuke@smuke.dev>
2021-09-24 15:21:56 -0700

    Map snowflake items

--- src/Components/SnowflakeContainer.jsx
+++ src/Components/SnowflakeContainer.jsx
@@ -2,16 +2,29 @@ import styled from "styled-components";
 
 import SnowflakeItem from "./SnowflakeItem";
 import AddSnowflakeItem from "./AddSnowflakeItem";
+import { useState } from "react";
 
 const Div = styled.div`
     margin-top: 200px;
 `;
 
 export default function SnowflakeContainer() {
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            value: ""
+        },
+        {
+            id: 2,
+            value: ""
+        }
+    ])
+
     return (
         <Div>
-            <SnowflakeItem />
-            <SnowflakeItem />
+            {items.map(item => {
+                return <SnowflakeItem key={item.id} />;
+            })}
             <AddSnowflakeItem />
         </Div>
     );
