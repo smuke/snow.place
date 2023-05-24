39f7d35ec6b2ce44e248edee70f4f65e8f504b69
smuke <smuke@smuke.dev>
2023-05-23 19:28:31 -0700

    Add snowflake item

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+import SnowflakeItem from "./components/SnowflakeItem";
+
+function App() {
+    return (
+        <>
+            <h1>Vite + React</h1>
+            <form>
+                <SnowflakeItem />
+            </form>
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/SnowflakeItem.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const SnowflakeItem = styled.div`
+    background: #1e2028;
+    width: 500px;
+    height: 50px;
+    border: 1px solid #2e313c;
+    border-radius: 5px;
+`;
+
+export default SnowflakeItem;
