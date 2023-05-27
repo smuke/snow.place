49d95cbade05a879fd8b959a39ad0858c7d0034b
smuke <smuke@smuke.dev>
2023-05-26 21:21:28 -0700

    Add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import styled from "styled-components";
 import "./styles.css";
 import SnowflakeContainer from "./components/SnowflakesContainer";
+import Logo from "./components/Logo";
+
+const Header = styled.header`
+    margin-bottom: 10px;
+`;
 
 function App() {
     return (
         <>
-            <h1>Snow.place</h1>
+            <Header>
+                <Logo />
+            </Header>
             <SnowflakeContainer />
         </>
     );
