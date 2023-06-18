c9390c008e68bd23970142f49123b8d7112626e6
smuke <smuke@smuke.dev>
2023-06-18 12:29:44 -0700

    Revert "Add heading"
    This reverts commit f1bb69fe5c5c3d8969aa45fd53fc05f2449d66b4.

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,7 @@ import SnowflakeContainer from "./components/SnowflakesContainer";
 import Logo from "./components/Logo";
 
 const Header = styled.header`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
-`;
-
-const Heading = styled.h1`
-    color: #8b94bb;
-    margin: 0;
-    padding: 0;
-    font: 400 1.1rem "Inter", sans-serif;
+    margin-bottom: 10px;
 `;
 
 function App() {
@@ -22,7 +12,6 @@ function App() {
         <>
             <Header>
                 <Logo />
-                <Heading>Compare Discord Timestamps</Heading>
             </Header>
             <SnowflakeContainer />
         </>
